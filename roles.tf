dafc4851b9d1e8d144dc643c4843f081eee751f1
Janik Muenk <janik.muenk@adesso.de>
2022-11-10 13:39:56 +0000

    adding option to reference a group id in azuread_groups_membership adding option to use services_roles for scopes shared_image_galleries and virtual_machine_scale_sets adding combined objects

--- roles.tf
+++ roles.tf
@@ -172,6 +172,7 @@ locals {
     storage_accounts                           = local.combined_objects_storage_accounts
     subscriptions                              = local.combined_objects_subscriptions
     synapse_workspaces                         = local.combined_objects_synapse_workspaces
+    virtual_machine_scale_sets                 = local.combined_objects_virtual_machine_scale_sets
     virtual_subnets                            = local.combined_objects_virtual_subnets
     virtual_machine_scale_sets                 = local.combined_objects_virtual_machine_scale_sets
     log_analytics                              = local.current_objects_log_analytics
