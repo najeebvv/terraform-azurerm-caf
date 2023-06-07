68937429a61a735dc5152301876ab239547b6437
Najeeb Vayal Valappil <najeeb.vayal-valappil@adesso.de>
2023-06-07 12:31:14 +0200

    feature: app gateway updates
    includes:
    
    - changes from upstream
    - default ssl policy
    - configurable policy per listener
    - fixed value reference in ssl_profile

--- modules/networking/application_gateway/main.tf
+++ modules/networking/application_gateway/main.tf
@@ -3,6 +3,9 @@ locals {
     "module" = basename(abspath(path.module))
   }
   tags = merge(var.base_tags, local.module_tag, try(var.settings.tags, null))
+
+  location            = var.resource_group.location
+  resource_group_name = var.resource_group.name
 }
 
 terraform {
