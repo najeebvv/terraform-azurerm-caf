4767250191bfe62cfb2f36cb95e9d1a715b93584
najeebvv <79649322+najeebvv@users.noreply.github.com>
2025-05-12 14:11:44 +0200

    Feat/ase virtual subnet (#73)
    * - virtual subnet support
    
    ---------
    
    Co-authored-by: Najeeb Vayal Valappil <najeeb.vayal-valappil.ext@flender-group.com>

--- app_service_environments.tf
+++ app_service_environments.tf
@@ -35,7 +35,7 @@ module "app_service_environments_v3" {
 
   global_settings     = local.global_settings
   settings            = each.value
-  subnet_id           = can(each.value.subnet_id) ? each.value.subnet_id : local.combined_objects_networking[try(each.value.lz_key, local.client_config.landingzone_key)][each.value.vnet_key].subnets[each.value.subnet_key].id
+  subnet_id           = can(each.value.subnet_id) ? each.value.subnet_id : coalesce(try(local.combined_objects_virtual_subnets[try(each.value.vnet.lz_key, local.client_config.landingzone_key)][each.value.vnet.subnet_key].id, null), try(local.combined_objects_networking[try(each.value.lz_key, local.client_config.landingzone_key)][each.value.vnet_key].subnets[each.value.subnet_key].id, null))
   diagnostic_profiles = try(each.value.diagnostic_profiles, {})
   diagnostics         = local.combined_diagnostics
   private_dns         = lookup(each.value, "private_dns_records", null) == null ? {} : local.combined_objects_private_dns
