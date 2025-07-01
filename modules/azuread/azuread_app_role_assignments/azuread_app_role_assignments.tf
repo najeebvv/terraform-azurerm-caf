ed09f3fdf134f14f0e70e80427067a0fd243b305
najeebvv <79649322+najeebvv@users.noreply.github.com>
2025-07-01 10:22:12 +0200

    support object id (#79)
    support object id in AD app role assignment

--- modules/azuread/azuread_app_role_assignments/azuread_app_role_assignments.tf
+++ modules/azuread/azuread_app_role_assignments/azuread_app_role_assignments.tf
@@ -4,6 +4,7 @@ resource "azuread_app_role_assignment" "app" {
   principal_object_id = coalesce(
     try(var.azuread_users[try(var.settings.azuread_user.lz_key, var.client_config.landingzone_key)][var.settings.azuread_user.key].object_id, null),
     try(var.azuread_groups[try(var.settings.azuread_group.lz_key, var.client_config.landingzone_key)][var.settings.azuread_group.key].object_id, null),
-    try(var.azuread_service_principals[try(var.settings.azuread_service_principal.lz_key, var.client_config.landingzone_key)][var.settings.azuread_service_principal.key].object_id, null)
+    try(var.azuread_service_principals[try(var.settings.azuread_service_principal.lz_key, var.client_config.landingzone_key)][var.settings.azuread_service_principal.key].object_id, null),
+    var.settings.object_id
   )
 }
